5e2e61f7f5ffae71eadb28ccd8fb84bceff19624
killerad123 <abhidesh1702@gmail.com>
2025-07-26 23:06:42 +0530

    updated code

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // This is a simple Java program that prints "Hello, World!" to the console.
     }
 }
